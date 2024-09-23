ac83e3a3261db1272c46dfa3a3c5f45b82aaafc5
KwTai <kokwaitai123@gmail.com>
2024-09-23 10:56:02 +0800

    Squashed commit of the following:
    commit 19320aa6cf9ae2cb466dcb21c8f1a2b4a6a818d6
    Author: HOW YONG-HENG <137473083+howyongheng0313@users.noreply.github.com>
    Date:   Mon Sep 23 10:43:34 2024 +0800
    
        Delete tempCodeRunnerFile.py
    
    commit b90203e6c76f1e73de78f1b686ede80eea7f4af6
    Author: HOW YONG-HENG <howyongheng0313@gmail.com>
    Date:   Mon Sep 23 10:41:19 2024 +0800
    
        Please only use Add(), View() and Search()
    
    commit 95a1c5f9f3bc0d8e2625f54d5028e5d176bcdf5c
    Merge: 373d0c0 7ed1b35
    Author: HOW YONG-HENG <howyongheng0313@gmail.com>
    Date:   Mon Sep 2 10:37:34 2024 +0800
    
        Merge branch 'main' into YongHeng
    
    commit 373d0c064811b97bf15c376ed7260a48aabf56e5
    Author: HOW YONG-HENG <howyongheng0313@gmail.com>
    Date:   Mon Sep 2 10:35:22 2024 +0800
    
        Update Base.py
    
        delete a row for demo
    
    commit 56fa199589398fb694d90cab4724c159715702a2
    Author: HOW YONG-HENG <howyongheng0313@gmail.com>
    Date:   Mon Sep 2 10:32:53 2024 +0800
    
        ghh

--- admin.py
+++ admin.py
@@ -2,7 +2,7 @@
 file_path = "admin.txt"
 
 # Open file in write mode
-with open(file_path, 'w') as file:
+with open(file_path, 'a') as file:
     # Write data to the file
     file.write("Admin User\n")
     file.write("Admin Name, Admin Password, Admin Email\n")
