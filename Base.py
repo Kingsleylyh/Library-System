f045c274de2af9588d38bd1fdfcbf5671d2c01b0
Kingsley <kingsleylyh0918@gmail.com>
2024-10-06 01:25:01 +0800

    Huge update

--- Base.py
+++ Base.py
@@ -1,7 +1,8 @@
 # PAGE TO CHOOSE USER TYPE
+from admin import *
 def user_type():
     print(
-        "Welcome to Brickfields Kuala Lumpur Community Library:\n"
+        "Welcome to Brickfields Kuala Lumpur Community Library login page:\n"
         "Please Select A User Type:\n"
         "   1. System Administrator\n"
         "   2. Librarian\n"
@@ -11,7 +12,7 @@ def user_type():
         try:
             user_choice = int(input("Enter your user type (1/2/3):")) # REQUEST INPUT FROM USER
             if user_choice == 1:
-                system_admin_page()
+                admin_login()
             elif user_choice == 2:
                 librarian_page()
             elif user_choice == 3:
@@ -21,39 +22,7 @@ def user_type():
         except ValueError:
             print("Invalid input. Please enter a number.")
 
-
-import os
-from AdminMIM import *
-# SYSTEM ADMIN PAGE
-def system_admin_page():
-    os.system('cls' if os.name == 'nt' else 'clear') # CLEAR THE TERMINAL HISTORY
-    print("Welcome to Brickfields Kuala Lumpur Community Library Admin Page:\n"
-          "-------------------------------------------------------------------\n"
-          "Member Information Management:\n"
-          " 1. Add New Member Information\n"
-          " 2. View All Member Information\n"
-          " 3. Search Member Information\n"
-          " 4. Edit Member Information\n"
-          " 5. Remove Member Information\n\n"   
-          "Librarian Information Management:\n"
-          " 6. Add New Librarian\n"
-          " 7. View All Librarian Information\n"
-          " 8. Search Librarian Information\n"
-          " 9. Edit Librarian Information\n"
-          " 10. Remove Librarian\n"
-          " 11. Logout"
-          )
-    librarian_choice = input("Enter the index of your choice: ")
-    if librarian_choice == 1:
-        add_member_to_database()
-    elif librarian_choice == 2:
-        view_member_in_database()
-    elif librarian_choice == 3:
-        search_member_from_database()
-    elif librarian_choice == 4:
-        edit_member_information()
-    elif librarian_choice == 5:
-        remove_member_from_database()
+user_type()
 
 
 from Librarian import *
@@ -81,6 +50,8 @@ def librarian_page():
     elif librarian_choice == 4:
         edit_book_information()
 
+librarian_page()
+
 
 # LIBRARY MEMBER PAGE
 def library_member_page():
@@ -94,4 +65,4 @@ def library_member_page():
           )
     librarian_member_choice = input("Enter your choice: ")
 
-user_type()
+library_member_page()
