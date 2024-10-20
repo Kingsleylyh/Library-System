cd82bf266a418ef832045bfb65469cde395b307d
H-M-Lai <llhmlhm6@gmail.com>
2024-10-20 18:04:00 +0800

    Update Member.py

--- member/Member.py
+++ member/Member.py
@@ -7,12 +7,12 @@ def view_book_in_catalogue():
 
     # Check if the catalogue file exists
     if not os.path.exists('librarian/catalogue.txt'):
-        print('Catalogue Does Not Exist.')
+        print('Catalogue does not exist.')
         return
 
     # Check if the catalogue file is empty
     elif os.path.getsize('librarian/catalogue.txt') == 0:
-        print('Catalogue Is Empty.')
+        print('Catalogue is empty.')
         return 
     
     else:
@@ -23,7 +23,7 @@ def view_book_in_catalogue():
 
             # Extract and print the header (first line)
             header = lines[0].strip() # Extract and clean the header (removes trailing spaces/newlines)
-            print('Catalogue List:\n')
+            print('Catalogue list:\n')
             print(header) # Print the header as the title of the catalogue
 
             # Print a separator line based on the longest line in the file
@@ -51,121 +51,117 @@ def view_book_in_catalogue():
         except Exception as e:
             # Handle any errors during file reading
             print("Error reading catalogue file:", e)
-            return # End function if an error occurs
+
+# Update member information
+def update_member_information():
+    # Clear terminal history
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    # Check if the file exists
+    if not os.path.exists('admin/memberdatabase.txt'):
+        print('Member is not registered.')
+        return
+
+    # Check if the file is empty
+    elif os.path.getsize('admin/memberdatabase.txt') == 0:
+        print('Record is empty.')
+        return
     
-    # Run end_choice function after displaying the catalogue
-    # return
-
-# # Update member information
-# def update_member_information():
-#     # Clear terminal history
-#     os.system('cls' if os.name == 'nt' else 'clear')
-
-#     # Check if the file exists
-#     if not os.path.exists('admin/memberdatabase.txt'):
-#         print('Member is not registered.')
-#         return
-
-#     # Check if the file is empty
-#     elif os.path.getsize('admin/memberdatabase.txt') == 0:
-#         print('Record is empty.')
-#         return
-    
-#     else:
-#         try:
-#             username = input("Enter Name: ")
-#             edited_profile = []
-#             with open("admin/memberdatabase.txt", "r") as memberfile:
-#                 members = memberfile.readlines()
+    else:
+        try:
+            username = input("Enter Name: ")
+            edited_profile = []
+            with open("admin/memberdatabase.txt", "r") as memberfile:
+                members = memberfile.readlines()
             
-#             for credentials in members:
-#                 stored_username, stored_userage, stored_dob, stored_regdate, stored_ic = credentials.strip().split(":")
-#                 if stored_username == username:
-#                     print(f"ID: {stored_username}\nName: {stored_userage}\nPassword: {stored_dob}\nRole: {stored_regdate}\nIC: {stored_ic}")
-#                     print("1. ID")
-#                     print("2. Name")
-#                     print("3. Password")
-#                     print("4. Role")
-#                     print("5. IC")
-#                     print("6. All")
-#                     choice = input("What would you like to edit?").strip()
+            for credentials in members:
+                stored_username, stored_userage, stored_dob, stored_regdate, stored_ic = credentials.strip().split(":")
+                if stored_username == username:
+                    print(f"ID: {stored_username}\nName: {stored_userage}\nPassword: {stored_dob}\nRole: {stored_regdate}\nIC: {stored_ic}")
+                    print("1. ID")
+                    print("2. Name")
+                    print("3. Password")
+                    print("4. Role")
+                    print("5. IC")
+                    print("6. All")
+                    choice = input("What would you like to edit?").strip()
 
-#                     new_username = stored_username
-#                     new_userage = stored_userage
-#                     new_dob = stored_dob
-#                     new_regdate = stored_regdate
-#                     new_ic = stored_ic
+                    new_username = stored_username
+                    new_userage = stored_userage
+                    new_dob = stored_dob
+                    new_regdate = stored_regdate
+                    new_ic = stored_ic
 
-#                     if choice == "1":
-#                         new_username = input("Enter new id: ")
-#                     elif choice == "2":
-#                         new_userage = input("Enter new name: ")
-#                     elif choice == "3":
-#                         new_dob = input("Enter new password: ")
-#                     elif choice == "4":
-#                         new_regdate = input("Enter new role: ")
-#                     elif choice == "5":
-#                         new_ic = input("Enter new ic: ")
-#                     elif choice == "6":
-#                         new_username = input("Enter new id: ")
-#                         new_userage = input("Enter new name: ")
-#                         new_dob = input("Enter new password: ")
-#                         new_regdate = input("Enter new role: ")
-#                         new_ic = input("Enter new ic: ")
-#                     else:
-#                         print("Invalid option.")
-#                         continue
+                    if choice == "1":
+                        new_username = input("Enter new id: ")
+                    elif choice == "2":
+                        new_userage = input("Enter new name: ")
+                    elif choice == "3":
+                        new_dob = input("Enter new password: ")
+                    elif choice == "4":
+                        new_regdate = input("Enter new role: ")
+                    elif choice == "5":
+                        new_ic = input("Enter new ic: ")
+                    elif choice == "6":
+                        new_username = input("Enter new id: ")
+                        new_userage = input("Enter new name: ")
+                        new_dob = input("Enter new password: ")
+                        new_regdate = input("Enter new role: ")
+                        new_ic = input("Enter new ic: ")
+                    else:
+                        print("Invalid option.")
+                        continue
 
-#                     edited_profile.append(f"{new_username}:{new_userage}:{new_dob}:{new_regdate}:{new_ic}\n")
-#                     print("Member information edited.")
-#                 else:
-#                     edited_profile.append(credentials)
+                    edited_profile.append(f"{new_username}:{new_userage}:{new_dob}:{new_regdate}:{new_ic}\n")
+                    print("Member information edited.")
+                else:
+                    edited_profile.append(credentials)
 
-#             with open("admin/memberdatabase.txt", "w") as memberfile:
-#                 memberfile.writelines(edited_profile)
+            with open("admin/memberdatabase.txt", "w") as memberfile:
+                memberfile.writelines(edited_profile)
 
-#         except Exception as e:
-#             print("Error reading memberdatabase file:", e)
+        except Exception as e:
+            print("Error reading memberdatabase file:", e)
 
 
-# # Search book
-# def search_book_from_database():
-#     # Check if catalogue file exists 
-#     if not os.path.exists('librarian/catalogue.txt'): 
-#         print('Catalogue does not exist.')
-#         return
+# Search book
+def search_book_from_database():
+    # Check if catalogue file exists 
+    if not os.path.exists('librarian/catalogue.txt'): 
+        print('Catalogue does not exist.')
+        return
     
-#     # Check if the catalogue file is empty
-#     elif os.path.getsize('librarian/catalogue.txt') == 0:
-#         print('Catalogue is empty.')
-#         return
+    # Check if the catalogue file is empty
+    elif os.path.getsize('librarian/catalogue.txt') == 0:
+        print('Catalogue is empty.')
+        return
 
-#     else:
-#         # Read all lines from the catalogue file
-#         with open('librarian/catalogue.txt', 'r') as catalogue:
-#             lines = catalogue.readlines() 
+    else:
+        # Read all lines from the catalogue file
+        with open('librarian/catalogue.txt', 'r') as catalogue:
+            lines = catalogue.readlines() 
 
-#         # Get search term from user input and normalize it (convert to lowercase and remove extra spaces)
-#         search_term = input('Please enter your search term:')
-#         search_term = search_term.lower().strip()
+        # Get search term from user input and normalize it (convert to lowercase and remove extra spaces)
+        search_term = input('Please enter your search term:')
+        search_term = search_term.lower().strip()
 
-#         # Initialize a list to store the found books
-#         found_books = []
+        # Initialize a list to store the found books
+        found_books = []
         
-#         # Loop through the catalogue lines (skip the first line, which is the header)
-#         for line in lines[1:]:
-#             lower_book_details = line.strip().lower()  # Convert book details to lowercase for case-insensitive search
-#             normal_book_details = line.strip()  # Keep the original format for display
+        # Loop through the catalogue lines (skip the first line, which is the header)
+        for line in lines[1:]:
+            lower_book_details = line.strip().lower()  # Convert book details to lowercase for case-insensitive search
+            normal_book_details = line.strip()  # Keep the original format for display
 
-#             # Check if the search term is found in the book details
-#             if search_term in lower_book_details:
-#                 found_books.append(normal_book_details)  # Add the book to the found_book list
+            # Check if the search term is found in the book details
+            if search_term in lower_book_details:
+                found_books.append(normal_book_details)  # Add the book to the found_book list
         
-#         return lines[0], found_books
+        return lines[0], found_books
 
 
-# def main():
-#     update_member_information()
+def main():
+    update_member_information()
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
